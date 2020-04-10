6674fedbf1205f316c9678bd7a19fbe372ae6183
Ricardo Daniel <rick_peder@hotmail.com>
2020-04-10 14:35:33 -0300

    Implementado a parte de Alteração e Busca de dados na tabela Clientes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.btnAlterar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
-            this.btnListar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             this.dgvLista = new System.Windows.Forms.DataGridView();
             this.lblNome = new System.Windows.Forms.Label();
             this.lblDataNasc = new System.Windows.Forms.Label();
@@ -41,59 +41,62 @@
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtTelefone = new System.Windows.Forms.MaskedTextBox();
             this.txtDataNasc = new System.Windows.Forms.MaskedTextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCadastrar
             // 
-            this.btnCadastrar.Location = new System.Drawing.Point(8, 287);
+            this.btnCadastrar.Location = new System.Drawing.Point(7, 310);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
-            this.btnCadastrar.TabIndex = 5;
+            this.btnCadastrar.TabIndex = 6;
             this.btnCadastrar.Text = "Cadastrar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
             this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
             // 
             // btnAlterar
             // 
-            this.btnAlterar.Location = new System.Drawing.Point(89, 287);
+            this.btnAlterar.Location = new System.Drawing.Point(88, 310);
             this.btnAlterar.Name = "btnAlterar";
             this.btnAlterar.Size = new System.Drawing.Size(75, 23);
-            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.TabIndex = 7;
             this.btnAlterar.Text = "Alterar";
             this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
             // 
             // btnExcluir
             // 
-            this.btnExcluir.Location = new System.Drawing.Point(170, 287);
+            this.btnExcluir.Location = new System.Drawing.Point(169, 310);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(75, 23);
-            this.btnExcluir.TabIndex = 7;
+            this.btnExcluir.TabIndex = 8;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
             // 
-            // btnListar
+            // btnBuscar
             // 
-            this.btnListar.Location = new System.Drawing.Point(251, 287);
-            this.btnListar.Name = "btnListar";
-            this.btnListar.Size = new System.Drawing.Size(75, 23);
-            this.btnListar.TabIndex = 8;
-            this.btnListar.Text = "Listar";
-            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Location = new System.Drawing.Point(88, 3);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(48, 23);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // dgvLista
             // 
             this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvLista.Location = new System.Drawing.Point(6, 131);
+            this.dgvLista.Location = new System.Drawing.Point(5, 147);
             this.dgvLista.Name = "dgvLista";
             this.dgvLista.Size = new System.Drawing.Size(324, 150);
-            this.dgvLista.TabIndex = 4;
+            this.dgvLista.TabIndex = 5;
             // 
             // lblNome
             // 
             this.lblNome.AutoSize = true;
             this.lblNome.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNome.Location = new System.Drawing.Point(5, 14);
+            this.lblNome.Location = new System.Drawing.Point(5, 36);
             this.lblNome.Name = "lblNome";
             this.lblNome.Size = new System.Drawing.Size(51, 17);
             this.lblNome.TabIndex = 5;
@@ -103,7 +106,7 @@
             // 
             this.lblDataNasc.AutoSize = true;
             this.lblDataNasc.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDataNasc.Location = new System.Drawing.Point(3, 40);
+            this.lblDataNasc.Location = new System.Drawing.Point(3, 62);
             this.lblDataNasc.Name = "lblDataNasc";
             this.lblDataNasc.Size = new System.Drawing.Size(133, 17);
             this.lblDataNasc.TabIndex = 6;
@@ -113,7 +116,7 @@
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.Location = new System.Drawing.Point(4, 69);
+            this.lblEmail.Location = new System.Drawing.Point(4, 91);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(52, 17);
             this.lblEmail.TabIndex = 7;
@@ -123,7 +126,7 @@
             // 
             this.lblTelefone.AutoSize = true;
             this.lblTelefone.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTelefone.Location = new System.Drawing.Point(4, 99);
+            this.lblTelefone.Location = new System.Drawing.Point(4, 121);
             this.lblTelefone.Name = "lblTelefone";
             this.lblTelefone.Size = new System.Drawing.Size(62, 17);
             this.lblTelefone.TabIndex = 8;
@@ -131,40 +134,49 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(71, 10);
+            this.txtNome.Location = new System.Drawing.Point(71, 32);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(258, 20);
-            this.txtNome.TabIndex = 0;
+            this.txtNome.TabIndex = 1;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(72, 69);
+            this.txtEmail.Location = new System.Drawing.Point(72, 91);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(258, 20);
-            this.txtEmail.TabIndex = 2;
+            this.txtEmail.TabIndex = 3;
             // 
             // txtTelefone
             // 
-            this.txtTelefone.Location = new System.Drawing.Point(71, 99);
+            this.txtTelefone.Location = new System.Drawing.Point(71, 121);
             this.txtTelefone.Mask = "(99) 0000-0000";
             this.txtTelefone.Name = "txtTelefone";
             this.txtTelefone.Size = new System.Drawing.Size(146, 20);
-            this.txtTelefone.TabIndex = 3;
+            this.txtTelefone.TabIndex = 4;
             // 
             // txtDataNasc
             // 
-            this.txtDataNasc.Location = new System.Drawing.Point(142, 36);
+            this.txtDataNasc.Location = new System.Drawing.Point(142, 58);
             this.txtDataNasc.Mask = "00/00/0000";
             this.txtDataNasc.Name = "txtDataNasc";
             this.txtDataNasc.Size = new System.Drawing.Size(146, 20);
-            this.txtDataNasc.TabIndex = 1;
+            this.txtDataNasc.TabIndex = 2;
             this.txtDataNasc.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(8, 5);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(71, 20);
+            this.txtId.TabIndex = 0;
+            this.txtId.Visible = false;
             // 
             // frmInicial
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 333);
+            this.ClientSize = new System.Drawing.Size(347, 363);
+            this.Controls.Add(this.txtId);
             this.Controls.Add(this.txtDataNasc);
             this.Controls.Add(this.txtTelefone);
             this.Controls.Add(this.txtEmail);
@@ -174,7 +186,7 @@
             this.Controls.Add(this.lblDataNasc);
             this.Controls.Add(this.lblNome);
             this.Controls.Add(this.dgvLista);
-            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.btnExcluir);
             this.Controls.Add(this.btnAlterar);
             this.Controls.Add(this.btnCadastrar);
@@ -193,7 +205,7 @@
         private System.Windows.Forms.Button btnCadastrar;
         private System.Windows.Forms.Button btnAlterar;
         private System.Windows.Forms.Button btnExcluir;
-        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnBuscar;
         private System.Windows.Forms.DataGridView dgvLista;
         private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.Label lblDataNasc;
@@ -203,6 +215,7 @@
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.MaskedTextBox txtTelefone;
         private System.Windows.Forms.MaskedTextBox txtDataNasc;
+        private System.Windows.Forms.TextBox txtId;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace CRUD_WindowsForms
 {
@@ -29,22 +30,91 @@ namespace CRUD_WindowsForms
 
                 MessageBox.Show("Cliente cadastrado com sucesso!");
                 LimparForm();
+                con.FecharConexao();
             }
             catch (Exception erro)
             {
                 MessageBox.Show("Erro no cadastro: " + erro.Message);
                 txtNome.Focus();
             }
-            
+
         }
 
         private void LimparForm()
         {
+            txtId.Clear();
             txtNome.Clear();
             txtDataNasc.Clear();
             txtEmail.Clear();
             txtTelefone.Clear();
             txtNome.Focus();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtId.Enabled = true;
+
+                string strSql = "UPDATE clientes SET nome=@nome, datanasc=@datanasc, email=@email, telefone=@telefone WHERE id = @id";
+                ConexaoBd con = new ConexaoBd(strSql);
+                con.Comando.Parameters.AddWithValue("@nome", txtNome.Text);
+                con.Comando.Parameters.AddWithValue("@datanasc", txtDataNasc.Text);
+                con.Comando.Parameters.AddWithValue("@email", txtEmail.Text);
+                con.Comando.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+                con.Comando.Parameters.AddWithValue("@id", MySqlDbType.Int32).Value = txtId.Text;
+                con.Comando.ExecuteNonQuery();
+
+                MessageBox.Show("Alteração efetuada com sucesso!");
+                LimparForm();
+                con.FecharConexao();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show("Erro: " + erro.Message);
+                txtNome.Focus();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            txtId.Visible = true;
+            txtId.Focus();
+            if (txtId.TextLength == 0)
+            {
+                MessageBox.Show("Preencha o campo ID para realizar a busca");
+            }
+            else
+            {
+                try
+                {
+                    string strSql = "SELECT * from clientes WHERE id=@id ";
+                    ConexaoBd con = new ConexaoBd(strSql);
+                    con.Comando.Parameters.AddWithValue("@id", MySqlDbType.Int32).Value = txtId.Text;
+                    MySqlDataReader dr = con.Comando.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                        txtNome.Text = dr["nome"].ToString();
+                        txtDataNasc.Text = dr["datanasc"].ToString();
+                        txtEmail.Text = dr["email"].ToString();
+                        txtTelefone.Text = dr["telefone"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("ID não localizado no Banco de Dados");
+                        LimparForm();
+                        txtId.Focus();
+                    }
+                    con.FecharConexao();
+                }
+                catch (Exception erro)
+                {
+                    MessageBox.Show("Erro na consulta: " + erro.Message);
+                    LimparForm();
+                    txtId.Focus();
+                }
+            }
         }
     }
 }
